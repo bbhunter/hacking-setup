43a11dbab3c168163079120cea2f1d11e1b7fbe5
Jonas Magnusson <magnusson1988@gmail.com>
2020-02-02 13:40:47 +0100

    Added all.txt from Jason Haddix

--- wordlists.sh
+++ wordlists.sh
@@ -14,3 +14,6 @@ git clone https://github.com/fuzzdb-project/fuzzdb.git /opt/fuzzdb || git -C /op
 
 # Commonspeak
 git clone https://github.com/assetnote/commonspeak2-wordlists.git /opt/commonspeak2 || git -C /opt/commonspeak2 pull
+
+# Jhaddix All
+wget https://gist.github.com/jhaddix/f64c97d0863a78454e44c2f7119c2a6a/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt -P /opt/jhaddix/
